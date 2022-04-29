f6e825e8cd35aa674fd69bd3072db0dec6e781e6
Jun <bepatient1@naver.com>
2022-04-29 17:11:53 +0900

    HTML

---
+++ 과제/6_2220110020_yoonseojun_2/assets/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+
+    $("#header_wrap").load("common_header.html");
+    $("#footer_wrap").load("common_footer.html");
+
+});
---
+++ 과제/6_2220110020_yoonseojun_final/assets/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(document).ready(function(){
+        $("#content").click(function(){
+            $(this).fadeOut(1000);
+            $("#s01").fadeIn(1000).addClass("db");
+        });
+    });
+});
